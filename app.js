8731de5a4842328ac7612f8c0cffd4b14b6cc46d
Abdul-Samad-75 <abdulsamad7523@gmail.com>
2024-12-20 20:20:59 +0530

    cors added

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.json())
 
 // Step 1: Configure CORS options (optional)
 const corsOptions = {
-    origin: 'https://prismatic-medovik-0a4ea3.netlify.app', // Replace with your frontend URL
+    origin: 'https://dapper-crumble-e286ff.netlify.app', // Replace with your frontend URL
     methods: 'GET,POST,PUT,DELETE', // Specify allowed methods
     allowedHeaders: 'Content-Type,Authorization', // Specify allowed headers
 };
